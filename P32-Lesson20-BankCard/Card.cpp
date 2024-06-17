efb8e839b6f17b368c302463a34bd9aec61734d8
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-17 18:19:43 +0300

    showInfo

--- P32-Lesson20-BankCard/Card.cpp
+++ P32-Lesson20-BankCard/Card.cpp
@@ -19,22 +19,34 @@ Card::Card()
 Card::Card(const char* client_name, double money)
 {
 	cardNumber = rand() + 10'000;
-	strcpy_s(name, 50, client_name); //!!!
+	strcpy_s(name, 50, client_name);	//!!!
 	strcpy_s(expiredDate, 6, "01.34");
 	PIN = rand() % 10'000;
-	cash = money;
+	cash = money;						//!!!
 
 #ifdef DEBUG
 	cout << "Card(): " << cardNumber << endl;
 #endif // DEBUG
 
-	cout << "Opened VIP card: " << cardNumber << endl;
+	cout << "Opened VIP card: " << cardNumber << endl; //!!!
 }
 
 Card::~Card()
 {
+#ifdef DEBUG
+	cout << "~Card(): " << cardNumber << endl;
+#endif // DEBUG
+	cout << "Closed card: " << cardNumber << endl;
 }
 
 void Card::showInfo()
 {
+	cout << "\n++++++++++++++++++++++++++++++++++\n";
+	cout << "|\tCard:     " << cardNumber << endl;
+	cout << "|\tClient:   " << name << endl;
+	cout << "|\tCash:     " << cash << " UAH" << endl;
+	cout << "++++++++++++++++++++++++++++++++++\n";
+	cout << "|\tExp date: " << expiredDate << endl;
+	cout << "|\tPIN:      " << PIN << endl;
+	cout << "++++++++++++++++++++++++++++++++++\n\n";
 }
