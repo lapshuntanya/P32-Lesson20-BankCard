020ade2b81296b93289c4e328834e380bca1c5d5
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-17 18:33:41 +0300

    main

--- P32-Lesson20-BankCard/P32-Lesson20-BankCard.cpp
+++ P32-Lesson20-BankCard/P32-Lesson20-BankCard.cpp
@@ -1,9 +1,26 @@
 #include "Card.h"
+#include <ctime>
 
 int main()
 {
-	Card a;
-	a.showInfo();
+	srand(time(0));
+	int user = 0;
+	Card* privat = nullptr;
+
+	cout << "0 - Standart card\n";
+	cout << "1 - VIP card\n";
+	cin >> user;
+
+	if (user == 0) {
+		privat = new Card();//Конструктор за замовченням
+	}
+	else {
+		privat = new Card("Lapshun Tetiana", 1'000); //Конструктор з параметрами
+	}
+
+	privat->showInfo();
 	 
+
+	delete privat;
 }
 
